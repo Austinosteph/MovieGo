f33cf4cf5f42fbd3fe7828c0c297e44b8832bbaf
Austinosteph <austinosteph@email.com>
2025-09-23 14:33:38 +0100

    fix: update default server port to 5000 in app.js

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use('/api/v1/booking', authenticateUser, bookingRouter);
 app.use('/api/v1/payment', paymentRouter);
 
 //start server with database
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const start = async () => {
 	try {
 		await connectDB(process.env.MONGO_URI);
